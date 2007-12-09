0b9b6e0a5e40a9ba6551d9a1b701e148cf4be6d9
lemonberry <lemonberry@79674033-b940-0410-9f37-2588eec0b5b4>
2007-12-09 21:34:44 +0000

    Version 0.2 - Torrent/File priorities implemented. - Various bug fixes/code cleanup - Changed style to white background. (ie, Web v2.0!)

--- rtgui/control.php
+++ rtgui/control.php
@@ -11,10 +11,22 @@ import_request_variables("gp","r_");
 
 if (isset($r_setmaxup) || isset($r_setmaxdown)) $r_cmd="setcap";
 if (isset($r_addurl)) $r_cmd="addurl";
+
+// Set file priorities...
 if (isset($r_set_fpriority)) {
    $index=0;
    foreach($r_set_fpriority as $item) {
       $response=do_xmlrpc(xmlrpc_encode_request("f.set_priority",array("$r_hash",$index,"$item")));
+      $index++;
+   }
+   $r_cmd="";
+}
+
+// Set torrent priorities...
+if (isset($r_set_tpriority)) {
+   $index=0;
+   foreach($r_set_tpriority as $item) {
+      $response=do_xmlrpc(xmlrpc_encode_request("d.set_priority",array($r_hash[$index],$item)));
       $index++;
    }
    $r_cmd="";
--- rtgui/view.php
+++ rtgui/view.php
@@ -31,7 +31,7 @@ echo "<table width=100% border=0 cellpadding=5 cellspacing=0>";
 echo "<tr><td><a href='index.php'><h1>rtGooey</h1></a>";
 echo "<i>The rTorrent Graphical User Interface</i></td>";
 echo "<td>&nbsp;</td>";      
-echo "</td></table>\n<br>\n";
+echo "</td></table>\n<br>\n\n";
 
 // Torrent info...  (get all downloads, then filter out just this one by the hash)
 $alltorrents=get_full_list("main");
@@ -39,8 +39,8 @@ $thistorrent=array();
 foreach($alltorrents as $torrent) {
    if ($r_hash==$torrent['hash']) $thistorrent=$torrent;
 }
-echo "<br>\n&laquo; <a href='index.php'>Back</a> | ";
-echo "<a href='control.php?hash=".$thistorrent['hash']."&cmd=".($thistorrent['is_active']==1 ? "stop" : "start")."'>".($thistorrent['is_active']==1 ? "Stop" : "Start")."</a> | <a href='control.php?hash=".$thistorrent['hash']."&cmd=delete' onClick='return confirm(\"Are you sure?\");'>Delete</a> | <a href='control.php?hash=".$thistorrent['hash']."&cmd=hashcheck'>Check</a><br>\n<br>\n";
+echo "<br>\n\n&laquo; <a href='index.php'>Back</a> | ";
+echo "<a href='control.php?hash=".$thistorrent['hash']."&cmd=".($thistorrent['is_active']==1 ? "stop" : "start")."'>".($thistorrent['is_active']==1 ? "Stop" : "Start")."</a> | <a href='control.php?hash=".$thistorrent['hash']."&cmd=delete' onClick='return confirm(\"Are you sure?\");'>Delete</a> | <a href='control.php?hash=".$thistorrent['hash']."&cmd=hashcheck'>Check</a><br>\n\n<br>\n\n";
 
 if ($thistorrent['complete']) { $statusflags="Complete "; } else { $statusflags="Incomplete ";}
 if ($thistorrent['is_hash_checked']) $statusflags.="&middot; Hash Checked ";
@@ -71,7 +71,7 @@ echo "<tr class='row2'><td align=right><b>Ratio:</b></td><td>".@round(($thistorr
 echo "<tr class='row1'><td align=right><b>Status:</b></td><td>".$thistorrent['status_string']."</td></tr>\n";
 echo "<tr class='row2'><td align=right><b>Priority:</b></td><td>".$thistorrent['priority_str']."</td></tr>\n";
 
-echo "</table>\n<br>\n";
+echo "</table>\n<br>\n\n";
 
 // tracker info...
 echo "<h2>Trackers:</h2>";
@@ -94,7 +94,7 @@ foreach($data AS $item) {
    echo "<td align=center>".($item['is_enabled']==1 ? "Yes" : "No")."</td>";
    if ($thisrow=="row1") {$thisrow="row2";} else {$thisrow="row1";}
 }
-echo "</table>\n<br>\n";
+echo "</table>\n<br>\n\n";
 
 // file info...
 echo "<h2>File List:</h2>";
@@ -114,20 +114,20 @@ foreach($data AS $item) {
    echo "<tr class='$thisrow'>";
    echo "<td>".$item['get_path']."</td>";
    echo "<td align=center>".format_bytes($item['get_size_bytes'])."</td>";
-   echo "<td align=center>".@round(($item['get_completed_chunks']/$item['get_size_chunks'])*100)." %<br>\n";
-    echo "<table border=0 cellspacing=0 cellpadding=1 bgcolor=#666666 width=50px><tr><td align=left>";
+   echo "<td align=center>".@round(($item['get_completed_chunks']/$item['get_size_chunks'])*100)." %<br>\n\n";
+    echo "<table border=0 cellspacing=0 cellpadding=1 bgcolor=#666666 width=50px><tr><td align=left>\n";
     echo "<img src='percentbar.gif' height=4px width=".@round((($item['get_completed_chunks']/$item['get_size_chunks'])*100)/2)."px>";
     echo "</td></tr>\n</table>\n";
-   echo "</td>";
-   echo "<td align=center>".$item['get_completed_chunks']." / ".$item['get_size_chunks']."</td>";
-   echo "<td>";
-   echo "<select name='set_fpriority[$index]'>";
-   echo "<option value='0' ".($item['get_priority']==0 ? "selected" : "").">Off</option>";
-   echo "<option value='1' ".($item['get_priority']==1 ? "selected" : "").">Normal</option>";
-   echo "<option value='2' ".($item['get_priority']==2 ? "selected" : "").">High</option>";
-   echo "</select>";
-   echo "<input type='hidden' name='hash' value='$r_hash'>";
-   echo "</td>";
+   echo "</td>\n";
+   echo "<td align=center>".$item['get_completed_chunks']." / ".$item['get_size_chunks']."</td>\n";
+   echo "<td>\n";
+   echo "<select name='set_fpriority[$index]'>\n";
+   echo "<option value='0' ".($item['get_priority']==0 ? "selected" : "").">Off</option>\n";
+   echo "<option value='1' ".($item['get_priority']==1 ? "selected" : "").">Normal</option>\n";
+   echo "<option value='2' ".($item['get_priority']==2 ? "selected" : "").">High</option>\n";
+   echo "</select>\n";
+   echo "<input type='hidden' name='hash' value='$r_hash'>\n";
+   echo "</td>\n";
    echo "</tr>\n";
    if ($thisrow=="row1") {$thisrow="row2";} else {$thisrow="row1";}
    $index++;
